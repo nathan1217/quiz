6782e440ae1e8ccba0202437a57994ce0d9737bd
Jiang <qiulin.jiang@sap.com>
2018-10-16 10:59:47 +0800

    first version

--- app/src/main/java/com/example/demo/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/demo/geoquiz/QuizActivity.kt
@@ -2,11 +2,27 @@ package com.example.demo.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
-
+    private var mTrueButton: Button = null
+    private var mFalseButton: Button = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        mTrueButton = this.findViewById(R.id.btnTrue);
+        mTrueButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = this.findViewById(R.id.btnFalse);
+        mFalseButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
