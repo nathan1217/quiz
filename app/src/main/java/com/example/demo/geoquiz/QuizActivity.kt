b8c733df22f20f49814c506259e2491c281641e5
Jiang <qiulin.jiang@sap.com>
2018-10-16 11:30:59 +0800

    fix bug

--- app/src/main/java/com/example/demo/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/demo/geoquiz/QuizActivity.kt
@@ -7,22 +7,25 @@ import android.widget.Button
 import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
-    private var mTrueButton: Button = null
-    private var mFalseButton: Button = null
+    private var mTrueButton: Button? = null
+    private var mFalseButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
-        mTrueButton = this.findViewById(R.id.btnTrue);
-        mTrueButton.setOnClickListener(object : View.OnClickListener {
+        mTrueButton = this.findViewById(R.id.btnTrue)
+        mTrueButton?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this@QuizActivity, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
-        });
-        mFalseButton = this.findViewById(R.id.btnFalse);
-        mFalseButton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
-            }
-        });
+        })
+        mFalseButton = this.findViewById(R.id.btnFalse)
+        mFalseButton?.setOnClickListener { _ ->
+            Toast.makeText(
+                this@QuizActivity,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
